8c22f880b5b951814b142291d67b298dc8a3bb1f
mogr20 <120605657+mogr20@users.noreply.github.com>
2025-02-04 14:25:13 +0000

    ADD: Daily weather API request

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -63,7 +63,7 @@ function getOneWeatherHour(dateTime, weatherData) {
     //error handling return here.
 }
 
-// Fetch request to get weather data from the API
+// Fetch request to get hourly weather data from the API
 function updateWeatherHour(lat,long) {
     // getCurrentLocation();
 
@@ -92,6 +92,42 @@ function updateWeatherHour(lat,long) {
         setTemperatureTextArea(currentWeatherHour);
         setUVIndexTextArea(currentWeatherHour);
         setWeatherDescriptionAndIcon(currentWeatherHour);
+
+    })
+    .catch(error => {
+        console.error('Error fetching weather data:', error);
+    });
+}
+
+// Fetch request to get daily weather data from the API
+function updateWeatherDaily(lat,long) {
+    // getCurrentLocation();
+
+    const url = `https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/daily`;
+    const apikey = `eyJ4NXQjUzI1NiI6Ik5XVTVZakUxTkRjeVl6a3hZbUl4TkdSaFpqSmpOV1l6T1dGaE9XWXpNMk0yTWpRek5USm1OVEE0TXpOaU9EaG1NVFJqWVdNellXUm1ZalUyTTJJeVpBPT0iLCJraWQiOiJnYXRld2F5X2NlcnRpZmljYXRlX2FsaWFzIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ==.eyJzdWIiOiJtb2d0aGVtb3NxdWl0b0BnbWFpbC5jb21AY2FyYm9uLnN1cGVyIiwiYXBwbGljYXRpb24iOnsib3duZXIiOiJtb2d0aGVtb3NxdWl0b0BnbWFpbC5jb20iLCJ0aWVyUXVvdGFUeXBlIjpudWxsLCJ0aWVyIjoiVW5saW1pdGVkIiwibmFtZSI6InNpdGVfc3BlY2lmaWMtMDViNTQ1ODAtYWYwZS00ZmJjLWI1ZjMtMmE5ZmViOWVlYjdjIiwiaWQiOjkzMzEsInV1aWQiOiI1NDgxMTc1Mi01NjlkLTQ4MWMtODU2Ny1iYTVhN2RlZGIzMzIifSwiaXNzIjoiaHR0cHM6XC9cL2FwaS1tYW5hZ2VyLmFwaS1tYW5hZ2VtZW50Lm1ldG9mZmljZS5jbG91ZDo0NDNcL29hdXRoMlwvdG9rZW4iLCJ0aWVySW5mbyI6eyJ3ZGhfc2l0ZV9zcGVjaWZpY19mcmVlIjp7InRpZXJRdW90YVR5cGUiOiJyZXF1ZXN0Q291bnQiLCJncmFwaFFMTWF4Q29tcGxleGl0eSI6MCwiZ3JhcGhRTE1heERlcHRoIjowLCJzdG9wT25RdW90YVJlYWNoIjp0cnVlLCJzcGlrZUFycmVzdExpbWl0IjowLCJzcGlrZUFycmVzdFVuaXQiOiJzZWMifX0sImtleXR5cGUiOiJQUk9EVUNUSU9OIiwic3Vic2NyaWJlZEFQSXMiOlt7InN1YnNjcmliZXJUZW5hbnREb21haW4iOiJjYXJib24uc3VwZXIiLCJuYW1lIjoiU2l0ZVNwZWNpZmljRm9yZWNhc3QiLCJjb250ZXh0IjoiXC9zaXRlc3BlY2lmaWNcL3YwIiwicHVibGlzaGVyIjoiSmFndWFyX0NJIiwidmVyc2lvbiI6InYwIiwic3Vic2NyaXB0aW9uVGllciI6IndkaF9zaXRlX3NwZWNpZmljX2ZyZWUifV0sInRva2VuX3R5cGUiOiJhcGlLZXkiLCJpYXQiOjE3Mzg2NjU1MTksImp0aSI6IjlmMDQ2MWJiLWFmODctNDc3MC1hYzRhLTMxN2RiODE1NjQxYyJ9.AEFlnjZrvfSlDBzGF90mZb5hH9yXQ-RLkNKBzOOJOs-tKJctS05iLIgL0SjlohsskiYhLphVG4tZ4qgqnR2X97mGjXjtzYALoaDQYI1IBXBN9MEAIdoqhPAMghzlkgOSzjyc7_NpB8pALsqOzHVdediOr4TVmQv04lEfwpPs8Lp2ALByGadWDzSBBzN-qLktCjRS0H-oHDCC2qHoKTF-p3svvyvah9hCWTG-HaWK1JcsEpAPIqD-uu2KtOxkSUq097WPKpNno1dybsISx6uBZSVeP7ejP0sH16omqk03fPHAgNv9aE5buhmTkCQDO46X-VBk_zmF9BNtqTbNUkes3g==`;
+
+    if(lat === undefined || long === undefined) {
+        lat = 52.5;
+        long = -1.9;
+    }
+
+    //fetch written by AI
+    fetch(`${url}?latitude=${lat}&longitude=${long}`, {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json',
+            'apikey': apikey
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        // Process the weather data here
+        // const currentWeatherHour = getOneWeatherHour(getCurrentHourISO(), getAllWeatherHours(data));
+
+        // setTemperatureTextArea(currentWeatherHour);
+        // setUVIndexTextArea(currentWeatherHour);
+        // setWeatherDescriptionAndIcon(currentWeatherHour);
 
     })
     .catch(error => {
@@ -197,4 +233,4 @@ function loadingScreen() {
 // getCurrentLocation();
 
 
-updateWeatherHour(52.5,-1.9);
+// updateWeatherHour(52.5,-1.9);
