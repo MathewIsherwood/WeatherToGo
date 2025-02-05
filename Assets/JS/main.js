08f8d8f73241210bf0c86d8fda9d64ecbc2e5861
mogr20 <120605657+mogr20@users.noreply.github.com>
2025-02-05 13:33:24 +0000

    UPDATE: Function comments, and functionality comments added and improved upon

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,23 +1,16 @@
-function getAllParameters(weatherData) {
-    return weatherData.parameters;
-}
-
+/**
+ * API Key
+ * for the Met Office API
+ */
 function getAPIKey() {
     // return `eyJ4NXQjUzI1NiI6Ik5XVTVZakUxTkRjeVl6a3hZbUl4TkdSaFpqSmpOV1l6T1dGaE9XWXpNMk0yTWpRek5USm1OVEE0TXpOaU9EaG1NVFJqWVdNellXUm1ZalUyTTJJeVpBPT0iLCJraWQiOiJnYXRld2F5X2NlcnRpZmljYXRlX2FsaWFzIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ==.eyJzdWIiOiJtb2d0aGVtb3NxdWl0b0BnbWFpbC5jb21AY2FyYm9uLnN1cGVyIiwiYXBwbGljYXRpb24iOnsib3duZXIiOiJtb2d0aGVtb3NxdWl0b0BnbWFpbC5jb20iLCJ0aWVyUXVvdGFUeXBlIjpudWxsLCJ0aWVyIjoiVW5saW1pdGVkIiwibmFtZSI6InNpdGVfc3BlY2lmaWMtMDViNTQ1ODAtYWYwZS00ZmJjLWI1ZjMtMmE5ZmViOWVlYjdjIiwiaWQiOjkzMzEsInV1aWQiOiI1NDgxMTc1Mi01NjlkLTQ4MWMtODU2Ny1iYTVhN2RlZGIzMzIifSwiaXNzIjoiaHR0cHM6XC9cL2FwaS1tYW5hZ2VyLmFwaS1tYW5hZ2VtZW50Lm1ldG9mZmljZS5jbG91ZDo0NDNcL29hdXRoMlwvdG9rZW4iLCJ0aWVySW5mbyI6eyJ3ZGhfc2l0ZV9zcGVjaWZpY19mcmVlIjp7InRpZXJRdW90YVR5cGUiOiJyZXF1ZXN0Q291bnQiLCJncmFwaFFMTWF4Q29tcGxleGl0eSI6MCwiZ3JhcGhRTE1heERlcHRoIjowLCJzdG9wT25RdW90YVJlYWNoIjp0cnVlLCJzcGlrZUFycmVzdExpbWl0IjowLCJzcGlrZUFycmVzdFVuaXQiOiJzZWMifX0sImtleXR5cGUiOiJQUk9EVUNUSU9OIiwic3Vic2NyaWJlZEFQSXMiOlt7InN1YnNjcmliZXJUZW5hbnREb21haW4iOiJjYXJib24uc3VwZXIiLCJuYW1lIjoiU2l0ZVNwZWNpZmljRm9yZWNhc3QiLCJjb250ZXh0IjoiXC9zaXRlc3BlY2lmaWNcL3YwIiwicHVibGlzaGVyIjoiSmFndWFyX0NJIiwidmVyc2lvbiI6InYwIiwic3Vic2NyaXB0aW9uVGllciI6IndkaF9zaXRlX3NwZWNpZmljX2ZyZWUifV0sInRva2VuX3R5cGUiOiJhcGlLZXkiLCJpYXQiOjE3Mzg2NjU1MTksImp0aSI6IjlmMDQ2MWJiLWFmODctNDc3MC1hYzRhLTMxN2RiODE1NjQxYyJ9.AEFlnjZrvfSlDBzGF90mZb5hH9yXQ-RLkNKBzOOJOs-tKJctS05iLIgL0SjlohsskiYhLphVG4tZ4qgqnR2X97mGjXjtzYALoaDQYI1IBXBN9MEAIdoqhPAMghzlkgOSzjyc7_NpB8pALsqOzHVdediOr4TVmQv04lEfwpPs8Lp2ALByGadWDzSBBzN-qLktCjRS0H-oHDCC2qHoKTF-p3svvyvah9hCWTG-HaWK1JcsEpAPIqD-uu2KtOxkSUq097WPKpNno1dybsISx6uBZSVeP7ejP0sH16omqk03fPHAgNv9aE5buhmTkCQDO46X-VBk_zmF9BNtqTbNUkes3g==`;
     return `eyJ4NXQjUzI1NiI6Ik5XVTVZakUxTkRjeVl6a3hZbUl4TkdSaFpqSmpOV1l6T1dGaE9XWXpNMk0yTWpRek5USm1OVEE0TXpOaU9EaG1NVFJqWVdNellXUm1ZalUyTTJJeVpBPT0iLCJraWQiOiJnYXRld2F5X2NlcnRpZmljYXRlX2FsaWFzIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ==.eyJzdWIiOiJ6NGQxNTkyZ0BzdHVkZW50cy5jb2RlaW5zdGl0dXRlLm5ldEBjYXJib24uc3VwZXIiLCJhcHBsaWNhdGlvbiI6eyJvd25lciI6Ino0ZDE1OTJnQHN0dWRlbnRzLmNvZGVpbnN0aXR1dGUubmV0IiwidGllclF1b3RhVHlwZSI6bnVsbCwidGllciI6IlVubGltaXRlZCIsIm5hbWUiOiJzaXRlX3NwZWNpZmljLTlmYTI0MWZhLTM0NDgtNGZlZi1iNDNjLWEwNWIwMDlhNWUwMiIsImlkIjo5MzY1LCJ1dWlkIjoiNjU4MzNkNGEtNTk3Yi00OWE1LTk5M2YtOWE1YTg0MGVkMDIzIn0sImlzcyI6Imh0dHBzOlwvXC9hcGktbWFuYWdlci5hcGktbWFuYWdlbWVudC5tZXRvZmZpY2UuY2xvdWQ6NDQzXC9vYXV0aDJcL3Rva2VuIiwidGllckluZm8iOnsid2RoX3NpdGVfc3BlY2lmaWNfZnJlZSI6eyJ0aWVyUXVvdGFUeXBlIjoicmVxdWVzdENvdW50IiwiZ3JhcGhRTE1heENvbXBsZXhpdHkiOjAsImdyYXBoUUxNYXhEZXB0aCI6MCwic3RvcE9uUXVvdGFSZWFjaCI6dHJ1ZSwic3Bpa2VBcnJlc3RMaW1pdCI6MCwic3Bpa2VBcnJlc3RVbml0Ijoic2VjIn19LCJrZXl0eXBlIjoiUFJPRFVDVElPTiIsInN1YnNjcmliZWRBUElzIjpbeyJzdWJzY3JpYmVyVGVuYW50RG9tYWluIjoiY2FyYm9uLnN1cGVyIiwibmFtZSI6IlNpdGVTcGVjaWZpY0ZvcmVjYXN0IiwiY29udGV4dCI6Ilwvc2l0ZXNwZWNpZmljXC92MCIsInB1Ymxpc2hlciI6IkphZ3Vhcl9DSSIsInZlcnNpb24iOiJ2MCIsInN1YnNjcmlwdGlvblRpZXIiOiJ3ZGhfc2l0ZV9zcGVjaWZpY19mcmVlIn1dLCJ0b2tlbl90eXBlIjoiYXBpS2V5IiwiaWF0IjoxNzM4NzU0MTIxLCJqdGkiOiJiZDc0NTNhNC1kYWFiLTQxM2EtOWVkMi1mOWZhMzc2MDBlZmIifQ==.VtadWMvaTxSBp55FKJDaX56Qz9jU9AofwaBA0-SfY7Z0mSqi1wNOonBGDH4y2ZucJMjp_kgwZEMR19P60Su8EaaSWbQque-3pARI2fgkrFcF99LJRj0OqwgU2B4G-U1xuEkqWIh7f0ZlFB-G1hiPg9-zzbdTvWLxyZkZy-77iPSFxbgn4aTNG8hKatdpXLRnVm9b___M1PpkXn25MDxey8_VKb4sTtXQ2VKv14DjNENRvr2_3HFape_qeTXFrSrzBT_N-pY2ecB7TQ_giDSdLZV0KePo_XNhWYD7KvKMCciRPri1UtEhnlXOPTqkcgw_pbXDF0gvSLBvDcZZkWTVLw==`;
 }
 
-// function getParameterSymbolText(parameterName, allParameters) {
-//     for (let parameter of allParameters) {
-//         if (parameter.name === parameterName) {
-//             return parameter.unit.symbol.type;
-//         }
-//     }
-
-//     //error handling return here.
-// }
-
-//geolocation api service - w3schools
+/**
+ * Geolocation API
+ * utilises w3schools code
+ */
 function getCurrentLocation() {
     let currentLocationObj = {};
     if (navigator.geolocation) {
@@ -28,16 +21,22 @@ function getCurrentLocation() {
     }
 }
 
+/**
+ * Geolocation functionality
+ * uses location to get the town name and set the weather data
+ */
 function usePosition(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
 
     console.log("Latitude: " + latitude + " Longitude: " + longitude);
     getTownName(latitude, longitude);
-    updateWeatherHour("London", { "long": longitude, "lat": latitude });
+    // updateWeatherHour("London", { "long": longitude, "lat": latitude });
 }
 
-//geolocation error handling - w3schools
+/**
+ * Geolocation API Error handling
+ */
 function showError(error) {
     switch (error.code) {
         case error.PERMISSION_DENIED:
@@ -55,7 +54,11 @@ function showError(error) {
     }
 }
 
-function getTownName(lat,long) {
+/**
+ * Geonames API
+ * reverse geocoding to get the town name from the latitude and longitude
+ */
+function getTownName(lat, long) {
     const url = `http://api.geonames.org/findNearbyPlaceName`;
     const username = `moglin`;
 
@@ -69,7 +72,6 @@ function getTownName(lat,long) {
         .then(response => response.json())
         .then(data => {
             // Process town name here
-            // console.log(data.geonames[0].name);
             setGPSLocation(data.geonames[0].name);
         })
         .catch(error => {
@@ -77,14 +79,26 @@ function getTownName(lat,long) {
         });
 }
 
+/**
+ * GPS Location text
+ * sets the town name to HTML text
+ */
 function setGPSLocation(townName) {
     document.getElementById('GPSLocation').innerText = townName;
 }
 
+/**
+ * Weather times object
+ * strips the object down to an array of weather times
+ */
 function getAllWeatherTimes(weatherData) {
     return weatherData.features[0].properties.timeSeries;
 }
 
+/**
+ * Weather hour object
+ * strips the array of objects down to a single weather hour object
+ */
 function getOneWeatherHour(dateTime, weatherData) {
     for (let weatherHour of weatherData) {
         if (weatherHour.time === dateTime) {
@@ -95,9 +109,10 @@ function getOneWeatherHour(dateTime, weatherData) {
     //error handling return here.
 }
 
-// Fetch request to get hourly weather data from the API.
-// Optional parameter longlat, if a longitude and latitude object
-// aren't provided, then the function will fetch the longlat from location name
+/**
+ * Update HTML with one hour weather data
+ * passes through the town name and optional longitude and latitude object to set the HTML
+ */
 function updateWeatherHour(townName, longlat = 0) {
     const weatherTime = "hourly";
 
@@ -108,9 +123,10 @@ function updateWeatherHour(townName, longlat = 0) {
     }
 }
 
-// Fetch request to get daily weather data from the API.
-// Optional parameter longlat, if a longitude and latitude object
-// aren't provided, then the function will fetch the longlat from location name
+/**
+ * Update HTML with one day weather data
+ * passes through the town name and optional longitude and latitude object to set the HTML
+ */
 function updateWeatherDaily(townName, longlat = 0) {
     const weatherTime = "daily";
 
@@ -121,7 +137,11 @@ function updateWeatherDaily(townName, longlat = 0) {
     }
 }
 
-//get long - lat for places in the uk based on the name of the place
+/**
+ * Longitude and Latitude API from town name
+ * fetches the longitude and latitude using the town name, from the Postcodes API,
+ * and passes this through to the next fetch for weather data
+ */
 function getLongLat(placeName, weatherTime) {
     const url = `https:api.postcodes.io/places?query=${placeName}`;
     let long = 0;
@@ -139,7 +159,7 @@ function getLongLat(placeName, weatherTime) {
 
                     // console.log(longLatObj);
                     setWeather(weatherTime, longLatObj);
-                    
+
                     break;
                 }
             }
@@ -149,6 +169,11 @@ function getLongLat(placeName, weatherTime) {
         });
 }
 
+/**
+ * Met Office API
+ * fetches the weather data from the Met Office API
+ * sets the HTML for hourly, or daily weather data
+ */
 function setWeather(weatherTime, longlat) {
     const url = `https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/${weatherTime}`;
     const apikey = getAPIKey();
@@ -165,7 +190,7 @@ function setWeather(weatherTime, longlat) {
         .then(data => {
             console.log(data);
             // Process the weather data here
-            if(weatherTime === "hourly") {
+            if (weatherTime === "hourly") {
                 const currentWeatherHour = getOneWeatherHour(getCurrentHourISO(), getAllWeatherTimes(data));
 
                 setTemperatureTextArea(currentWeatherHour);
@@ -183,7 +208,10 @@ function setWeather(weatherTime, longlat) {
         });
 }
 
-// get the current hour (rounded to nearest hour) in ISO format
+/**
+ * Current Hour in ISO format
+ * gets the current time, and rounds to nearest item, ready for comparison
+ */
 function getCurrentHourISO() {
     const currentTime = new Date();
     let textTime = currentTime.toISOString();
@@ -214,17 +242,24 @@ function getCurrentHourISO() {
  * Current hour forecast section
  */
 function setTemperatureTextArea(weatherHour) {
+    // get the temperature from the weather object
     const temperature = Math.floor(weatherHour.screenTemperature).toString() + "°C";
+    // and set that temperature to the HTML text
     document.getElementsByClassName("TemperatureTextArea")[0].innerText = temperature;
 }
 
 function setUVIndexTextArea(weatherHour) {
+    // get the UV index from the weather object
     const uvIndex = "UV Index: " + weatherHour.uvIndex.toString();
+    // and set that UV Index to the HTML text
     document.getElementsByClassName("UVIndexTextArea")[0].innerText = uvIndex;
 }
 
 function setWeatherDescriptionAndIcon(weatherHour) {
+    // get the significant weather code from the weather object
+    // and do a lookup in the function to get a description and icon object
     const weatherInfo = getWeatherDescriptionAndIcon(weatherHour.significantWeatherCode);
+    // finally set that description and icon object to the HTML text and image
     document.getElementById("BottomTextArea").innerText = weatherInfo.description;
     document.getElementById("MainIcon").src = weatherInfo.icon;
     document.getElementById("MainIcon").innerHTML = `<img src="${weatherInfo.icon}" alt="Weather Icon">`;
@@ -237,8 +272,9 @@ function setFiveDayTemperatureTextArea(weatherDays) {
     //we skip the zeroith in the array, as that is yesterday's data
     let temperature = "";
     for (let i = 1; i < 6; i++) {
+        // get the maximum daytime temperature from the weather object
         temperature = Math.floor(weatherDays[i].dayMaxScreenTemperature).toString() + "°C";
-        //TODO: currently the dom is on index.html, not 5dayforecast.html
+        // and set that temperature to the HTML text
         document.getElementsByClassName(`Day${i}Temp`)[0].innerText = temperature;
     }
 }
@@ -248,8 +284,10 @@ function setFiveDayWeatherDescriptionAndIcon(weatherDays) {
     //we skip the zeroith in the array, as that is yesterday's data
     let weatherInfo = "";
     for (let i = 1; i < 6; i++) {
+        // get the significant weather code from the weather object
+        // and do a lookup in the function to get a description and icon object
         weatherInfo = getWeatherDescriptionAndIcon(weatherDays[i].daySignificantWeatherCode);
-        //TODO: currently the dom is on index.html, not 5dayforecast.html
+        // finally set that description and icon object to the HTML text and image
         document.getElementsByClassName(`Day${i}Weather`)[0].innerText = weatherInfo.description;
         document.getElementsByClassName(`Day${i}Icon`)[0].src = weatherInfo.icon;
     }
@@ -262,8 +300,13 @@ function setFiveDayWeatherDescriptionAndIcon(weatherDays) {
 // const text-orange-500; // Bit Warm
 // const text-red-500; // Really Warm
 
+/**
+ * Weather Description and Icon object
+ * passes through the weather code, and grabs the relevant description and icon
+ * for that weather code (a lookup table)
+ */
 function getWeatherDescriptionAndIcon(weatherCode) {
-    console.log("weatherCode"+weatherCode);
+    console.log("weatherCode" + weatherCode);
     const weatherMap = {
         0: { description: "Clear night", icon: "Assets/Images/clear_moon_night.png" },
         1: { description: "Sunny", icon: "Assets/Images/sunny.png" },
@@ -294,11 +337,17 @@ function getWeatherDescriptionAndIcon(weatherCode) {
         27: { description: "Heavy snow", icon: "Assets/Images/snow.png" },
         28: { description: "Thunder shower", icon: "Assets/Images/thundershowernight.png" },
         29: { description: "Thunder shower", icon: "Assets/Images/thundershower.png" },
-        30: { description: "Thunder", icon: "Assets/Images/thunder.png"}
+        30: { description: "Thunder", icon: "Assets/Images/thunder.png" }
     };
+    // return the description and icon for the numeric code. If the code cannot be found, return an error handling object.
     return weatherMap[weatherCode] || { description: "Unknown", icon: "Assets/Images/unknown.png" };
 }
 
+/**
+ * Loading Screen
+ * displays a loading screen while geo-location is being fetched
+ * takes 1-8 seconds...
+ */
 function loadingScreen() {
     const iconDisplayArea = document.getElementById('IconDisplayArea');
     iconDisplayArea.innerHTML = `
@@ -314,10 +363,14 @@ function loadingScreen() {
 }
 
 
-//html input box for placename refreshed upon keypress or submit button click
+/**
+ * Event listener
+ * listens for location to be submitted via enter or button click
+ */
 const placeName = document.getElementById('placeName');
 placeName.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
+        // get the weather data for the placename
         updateWeatherHour(placeName.value);
     }
 });
@@ -328,9 +381,12 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
+                // get the placename from the text box
                 let userAnswer = (document.getElementById("placeName").value).toString();
+                // and get the weather data for that placename
                 updateWeatherHour(userAnswer);
             } else {
+                // TODO: display this error message to the user
                 ErrorEvent("Unknown place submitted");
             }
         });
@@ -338,8 +394,13 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Toggle between GPS and Location Search
-
+/**
+ * Event Listener
+ * toggle button is clicked to switch between GPS location and searchbar
+ */
 document.getElementById('toggleButton').addEventListener('click', function () {
+    // TODO: when toggling back to GPS, if the gps previously failed
+    // (perhaps user refused), we need to try again, otherwise use existing data to save load time
     var searchbar = document.getElementById('searchbar');
     var gpsLocation = document.getElementById('GPSlocation');
     if (searchbar.style.display === 'none') {
@@ -353,13 +414,17 @@ document.getElementById('toggleButton').addEventListener('click', function () {
 
 
 // updateWeatherHour("Bristol");
+// Run the geolocation service, and set the weather for that location
 getCurrentLocation();
 
-// Initialize with searchbar visible and GPS location hidden
+// Initialize with GPS location visible and search bar hidden
 document.getElementById('searchbar').style.display = 'none';
 document.getElementById('GPSlocation').style.display = 'flex';
 
-// Switch background colour based on weather condition
+/**
+ * Background Temperature to Colour
+ * changes the background colour depending on the temperature at the location
+ */
 function switchBackgroundColour(weatherCondition) {
     switch (weatherCondition) {
         case 'Coldest':
