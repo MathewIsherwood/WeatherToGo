7701829a8ac7fbbd406a7351e0cbb1e8617a4c0e
Paveway-RC <ralfcrawshaw74@gmail.com>
2025-02-05 09:17:44 +0000

    Moved content of colourswitch.js to main.js. Deleted colourswitch.js as it is obselete.

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -251,3 +251,31 @@ let lat = 0;
 
 
 updateWeatherHour("Bristol");
+
+
+// Ensures that the background of the page changes depending on temperature
+function switchBackgroundColour(weatherCondition) {
+    switch (weatherCondition) {
+        case 'Coldest':
+            document.getElementById('primarybackground').className = 'TemperatureBackgroundColourColdest';
+            break;
+        case 'Cold':
+            document.getElementById('primarybackground').className = 'TemperatureBackgroundColourCold';
+            break;
+        case 'OK':
+            document.getElementById('primarybackground').className = 'TemperatureBackgroundColourOK';
+            break;
+        case 'Nice':
+            document.getElementById('primarybackground').className = 'TemperatureBackgroundColourNice';
+            break;
+        case 'BitWarm':
+            document.getElementById('primarybackground').className = 'TemperatureBackgroundColourBitWarm';
+            break;
+        case 'ReallyWarm':
+            document.getElementById('primarybackground').className = 'TemperatureBackgroundColourReallyWarm';
+            break;
+        default:
+            document.getElementById('primarybackground').className = 'TemperatureBackgroundColour';
+            break;
+    }
+}
